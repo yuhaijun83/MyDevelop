20b4fd913b2af3f05fd3f9ffc4a07731b1c9805f
Yu Haijun <yuhaijun83.dev@gmail.com>
2018-10-04 10:22:57 +0900

    Hello World!

---
+++ src/cn/com/yuhaijun/Main.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package cn.com.yuhaijun;
+
+/**
+ * @author kaigun.u
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
